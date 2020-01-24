d79dcfa5e820e8f1caf4c73d813803eda89f0b89
Rajasekaran Selvaraj <rajasekaran.s@kgisl.com>
2020-01-24 13:07:49 +0530

    testbasic

--- r.bat
+++ r.bat
@@ -1,2 +1,2 @@
 call mvn install
-java -cp target\brokermvc-1.0-SNAPSHOT.jar com.kgisl.brokermvc.App
+java -cp target\brokermvc-1.0-SNAPSHOT.jar com.kgisl.brokermvc.TestBasic
--- src/main/java/com/kgisl/brokermvc/TestBasic.java
+++ src/main/java/com/kgisl/brokermvc/TestBasic.java
@@ -1,7 +1,14 @@
 package com.kgisl.brokermvc;
 
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class TestBasic{
     public static void main(String[] args) {
-        In
+        Integer[] nums = {3,4,5,6,2};
+        Comparator intComparator = Comparator.comparing(Integer::intValue);
+        Integer minVal = (Integer)Stream.of(nums).min(intComparator).get();
+        System.out.println(minVal);
+
     }
 }
