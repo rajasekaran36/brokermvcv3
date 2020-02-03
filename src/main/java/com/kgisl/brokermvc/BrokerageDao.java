57e701e990b9cdebd9c96befd9ac41eff72f058e
Rajasekaran Selvaraj <rajasekaran.s@kgisl.com>
2020-02-03 15:46:05 +0530

    interface updated

--- src/main/java/com/kgisl/brokermvc/BrokerageDao.java
+++ src/main/java/com/kgisl/brokermvc/BrokerageDao.java
@@ -1,5 +1,6 @@
 package com.kgisl.brokermvc;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BrokerageDao{
@@ -7,4 +8,13 @@ public interface BrokerageDao{
     public void displayBrokerages();
     public void printContract(Integer id);
     public void printAllContracts();
+    public String topBuyScrip();
+    public String topSellScrip();
+    public Double totalBrokerage();
+    public Double totalBrokerageForDay(LocalDate date);
+    public LocalDate getHighestBrokerageCollectionDay();
+    public Double getTotalBrokerageForAPerson(String uccCode);
+    public LocalDate getHighestBrokeragePaidDayByClient(String uccCode);
+    public List<String> getDailyTradingUsers();
+    
 }
